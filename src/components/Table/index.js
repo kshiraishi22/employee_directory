c10a70f35e7139d0b3ea857ca0b3ae7c3998acc8
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-04 00:29:12 -0500

    added API call for random employee profiles

---
+++ src/components/Table/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  state = {
+    image: 
+    name: "",
+    phone: number,
+    email: "",
+    DOB: number
+  }
+
+
+render() {
+  return (
+    <div>
+
+    </div>
+    )
+  }
+}
+
+export default Table;
