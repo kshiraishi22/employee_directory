fe885196284734208d04f5695b67e14863f0ac48
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-05 16:47:18 -0500

    added input change function and filter to filter out data based on name search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import './App.css';
-import MainTable from "./components/MainTable"
+import React from "react";
+import "./App.css";
+import MainTable from "./components/MainTable";
 
 function App() {
   return (
-    <div>
+    <div className="container">
+
       <div className="Header">
         <h1>Employee Directory</h1>
         <p>Use search box to narrow your results!</p>
       </div>
-      <form>
-        <div className="row">
-          <div className="col-3">
-            <input type="text" className="form-control" placeholder="Search">
-            </input>
-          </div>
-          <button type="button" className="btn btn-primary">Search</button>
-          <MainTable />
-    
-        </div>
-      </form>
+      
+      <MainTable />
     </div>
   );
 }
--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -1,65 +1,104 @@
 import React from "react";
-import API from "../utils/API"
+import API from "../utils/API";
 
 const columns = [
-  {id: 1, label: "img"},
-  {id: 2, label: "name"},
-  {id: 3, label: "phone"},
-  {id: 4, label: "email"},
-  {id: 5, label: "DOB"},
-]
+  { id: 1, label: "image" },
+  { id: 2, label: "name" },
+  { id: 3, label: "phone" },
+  { id: 4, label: "email" },
+  { id: 5, label: "DOB" }
+];
 
 class MainTable extends React.Component {
   state = {
     results: [],
-    search: ""
+    filtered: [],
+    filter: ""
   };
 
   componentDidMount() {
-    this.searchEmployee()
+    this.searchEmployee();
   }
 
   searchEmployee = query => {
     API.getRandomEmployee(query)
       .then(res => {
-        console.log(res)
-        this.setState({ results: res.data.results })
-        console.log(res.data)
+        console.log(res);
+        this.setState({
+          results: res.data.results,
+          filtered: res.data.results
+        });
+        console.log(res.data);
       })
       .catch(err => console.log(err));
-  }
+  };
 
+  handleFilter = event => {
+    const { value } = event.target;
 
-render() {
-  return (
-    <>
-      <div className="container">
+    const filtered = this.state.results.filter(person => {
+      if (person.name.last.includes(value)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    if(filtered.length === 1){
+      console.log(Object.values(filtered[0]))
+    }
+
+    this.setState({ filter: value, filtered: filtered });
+  };
+
+  render() {
+    // var data = {
+    //   name: "ben",
+    //   age: 31
+    // };
+
+    // var myArr = ["dog", "cat"];
+    // console.log(myArr[0]);
+    // var key = "name";
+    // console.log(data[key]);
+    // key = "age";
+    // console.log(data[key]);
+
+    return (
+      <>
         <div className="row">
-          <div className="col-2">
+          <input
+            type="text"
+            className="form-control"
+            onChange={this.handleFilter}
+            value={this.state.filter}
+            placeholder="Search"
+          />
+          <div className="col-12">
             <table className="table">
               <thead>
                 <tr>
-                  { columns.map(column => (
-                  <th key={column.id}>{column.label}</th> ))}
+                  {columns.map(column => (
+                    <th key={column.id}>{column.label}</th>
+                  ))}
                 </tr>
               </thead>
               <tbody>
-                { this.state.results.map(person => (
-                <tr>
-                  <td><img src={person.picture.thumbnail} /></td>
-                  <td>{person.name.first} {person.name.last}</td>
-                  <td>{person.phone}</td>
-                  <td>{person.email}</td>
-                  <td>{person.dob.date}</td>
-                </tr>
+                {this.state.filtered.map(person => (
+                  <tr>
+                    <td><img src={person.picture.thumbnail} alt="#" /></td>
+                    <td>{person.name.first} {person.name.last}</td>
+                    <td>{person.phone}</td>
+                    <td>{person.email}</td>
+                    <td>{person.dob.date}</td>
+                  </tr>
                 ))}
               </tbody>
             </table>
           </div>
         </div>
-      </div>
-    </>
-    )
+      </>
+    );
   }
 }
-export default MainTable
+export default MainTable;
